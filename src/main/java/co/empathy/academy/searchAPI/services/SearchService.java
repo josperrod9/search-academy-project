da5d83c09b1799ac6d154ca1866403675fec7044
José Pérez Rodríguez <josep@josep.local>
2023-02-20 20:48:13 +0100

    Delete comment on SearchService.java

--- src/main/java/co/empathy/academy/searchAPI/services/SearchService.java
+++ src/main/java/co/empathy/academy/searchAPI/services/SearchService.java
@@ -5,8 +5,5 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface SearchService {
-    /*
-    void makeAggsQuery() throws IOException;*/
-
     QueryResponse search(String query);
 }
